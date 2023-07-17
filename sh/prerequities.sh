0a23bbb5e0e93c002f9931bde9c5f4a6a6cdd9e5
development <dev@ferrumgate.com>
2023-07-17 12:41:24 +0300

    br_netfilter

--- sh/prerequities.sh
+++ sh/prerequities.sh
@@ -46,6 +46,7 @@ prerequities() {
     ####
     info "load ipvs modules, and netfilter modules"
     cat <<EOF | tee /etc/modules-load.d/ipvs.conf
+br_netfilter
 ip_vs
 ip_vs_rr
 ip_vs_wrr
@@ -59,7 +60,7 @@ nf_conntrack
 EOF
 
     #### load modules to kernel
-    #modprobe -- br_netfilter
+    modprobe -- br_netfilter
     modprobe -- ip_vs
     modprobe -- ip_vs_rr
     modprobe -- ip_vs_wrr
