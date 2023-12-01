5daca4fb19c440095015d6f413d1aa80e8c17285
ferrum <ferrum@ferrumgate.com>
2023-12-02 00:17:04 +0300

    added ntp prerequities

--- sh/prerequities.sh
+++ sh/prerequities.sh
@@ -42,7 +42,8 @@ prerequities() {
         net-tools \
         sysstat \
         xxd \
-        conntrack ntp
+        conntrack \
+        ntp
 
     ####
     info "load ipvs modules, and netfilter modules"
